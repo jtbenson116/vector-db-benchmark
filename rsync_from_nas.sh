4e50f15b564121690a7ea9889c57d2f383778d58
jacobenson <jbenson116@gmail.com>
2024-02-14 23:37:37 +0000

    sync weaviate results from nas updated results notebook

--- rsync_from_nas.sh
+++ rsync_from_nas.sh
@@ -7,4 +7,17 @@ USER=jacob
 SOURCEDIR="/mnt/nas2/$USER/vdbenchmark_results/"
 SOURCE="$USER@192.168.99.40:$SOURCEDIR"
 mkdir -p ./results
-rsync -azvd0 --no-owner --no-group --no-perms "$SOURCE" ./results/
+rsync -azvd0 "$SOURCE" ./results/
+
+SOURCEDIR="/mnt/nas2/$USER/weaviate_results/"
+SOURCE="$USER@192.168.99.40:$SOURCEDIR"
+rsync -azvd0 "$SOURCE" ./results/
+
+SOURCEDIR="/mnt/nas1/fvs_benchmark_datasets/deep-"
+SOURCE="$USER@192.168.99.40:$SOURCEDIR"
+declare -a sizes=("10M" "50M" "100M")
+mkdir -p ./data
+for size in "${sizes[@]}"
+do
+    rsync -azvd0 "$SOURCE""$size"-gt-1000.npy ./data/
+done
