5d8d03673611f7cb81c3223b8d736598c5f45563
George Williams <george.williams@gmail.com>
2024-03-27 02:36:45 +0000

    updated rsync scripts related to nas2/george target

--- rsync_from_apu11_results_to_nas2_george.sh
+++ rsync_from_apu11_results_to_nas2_george.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 USER=gwilliams
-TARGETDIR="/mnt/nas2/jacob/vdbenchmark_results/last_validation_3-25-2024_results/"
+TARGETDIR="/mnt/nas2/george/vdbenchmark_results/"
 TARGET="$USER@192.168.99.40:$TARGETDIR"
 rsync -azvd0  ./results/ "$TARGET"
 
